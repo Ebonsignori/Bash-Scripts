0c32b63498ea8fc77a72a47565c2740f0a52e58a
Evan Bonsignori <evanbonsignori@Evans-MacBook-Pro.local>
2018-11-17 17:11:31 -0500

    Add first script and description README

---
+++ 1_list_of_names.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+#declare -a name_array=("grace" "evan" "longname" "bob" "nral")
+
+
+## Prompt and read in array of names separated by spaces
+echo "Enter a list of names separated by characters."
+read -a name_array
+
+## If the user didn't enter anything, prompt until they do
+declare -i input_length=${#name_array[@]}
+while [[ "$input_length" -le 0 ]]
+do
+ echo "Please enter at least one name"
+ read -a name_array
+ input_length=${#name_array[@]}
+done
+
+## Initialize variables
+declare -i var=0
+declare -a number_post_fixes=("st" "nd" "rd" "th")
+largest_name=""
+declare -i largest_count=0
+
+## Loop through each name
+for name in "${name_array[@]}"
+do
+    ## Get current number post_fix
+    if [[ "$var" -lt 4 ]]
+    then
+      post_fix=${number_post_fixes[${var}]}
+    fi
+
+    ## Increment and print results
+    var+=1
+    echo "$name is the ${var}${post_fix} name in the list with ${#name} characters"
+
+    if [[ "${#name}" -gt ${largest_count} ]]
+    then
+        largest_count=${#name}
+        largest_name=${name}
+    fi
+done
+
+echo ""
+echo "Largest name was '$largest_name' with $largest_count characters"
---
+++ README.md
@@ -0,0 +1,4 @@
+# Bash Scripting
+This repository contains 5 shell scripts whose functionality are described in the following image:
+[!Script Descriptions](./script_descriptions.png)
+
